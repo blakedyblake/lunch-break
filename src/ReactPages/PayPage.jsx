7077659b52a82c250eee95d0ec1c1a3e15ec2fa0
старейшина <blakedyblake@gmail.com>
2021-12-02 08:35:05 -0700

    fixed import folders for useContext

--- src/ReactPages/PayPage.jsx
+++ src/ReactPages/PayPage.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
-import { useContext } from "react/cjs/react.development";
+import { useContext } from "react";
 import BackButton from "../components/BackButton";
 import { GlobalContext } from "../Context/IdProvider";
 
